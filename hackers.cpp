75a863918df9a06e23e94049bf0d0a701223be46
Unknown <erick-espana97@live.com.mx>
2018-07-05 12:33:14 -0500

    Principio lol

---
+++ hackers.cpp
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct hackers{
+	
+	char nombre[30];
+	int ip;
+	struct hackers *siguiente;
+
+};
+
+void menu(){
+	
+	printf("MENU:/N1: Agregar/n2: Elimar/n3: Mostrar/n4: Salir/n");
+	
+}
+
+struct hackers *primero, *ultimo;
+
+void añadir_hacker(){
+	
+	nuevo = (struct hackers *) malloc (sizeof(struct hackers));
+	if()
+	printf("Ingresar Nuevo Elemento:/n");
+	printf("Ingresar Nombre:");
+	
+	struct hackers *nuevo;
+	
+	
+	
+}
+
+int main()
+{
+	menu();
+}
+
